e93a18d91f6512ff37a0fed5cce86cbb52c02d32
edsonplz <edson.silva@dcx.ufpb.br>
2026-04-08 08:52:33 -0300

    Recuperar e Salvar Contatos no Local storage Configurados

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const search = document.getElementById('search');
 const phone = document.getElementById('phone');
 const exportButton = document.getElementById('export-btn');
 const editIndex = document.getElementById('edit-index');
+
+// Recuperar contatos do Local Storage ou Inicializar com um array vazio
+let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+
+// Salvar contatos no Local Storage
+function saveContacts() {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+}
