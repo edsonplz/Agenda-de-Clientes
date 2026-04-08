e559ec4ee219a04326723bdaa94e622c45cadfbb
edsonplz <edson.silva@dcx.ufpb.br>
2026-04-08 08:48:14 -0300

    Pegando os elementos do DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+// Pegando os elementos do DOM
+
+const form = document.getElementById('contact-form');
+const contactList = document.getElementById('contact-list');
+const saveButton = document.getElementById('save-btn');
+const search = document.getElementById('search');
+const phone = document.getElementById('phone');
+const exportButton = document.getElementById('export-btn');
+const editIndex = document.getElementById('edit-index');
