f67565cec5bec91438f366f0929a1c27f241c47a
edsonplz <edson.silva@dcx.ufpb.br>
2026-04-09 11:22:17 -0300

    Aplicando mascara de telefone

--- script.js
+++ script.js
@@ -39,3 +39,25 @@ function renderContacts(filter = '') {
         }
     });
 }
+
+//Aplicar mascara de telefone
+document.addEventListener("DOMContentLoaded", function() {
+    if(phone) {
+        //Adicionando um evento de input para o campo de telefone
+        phone.addEventListener("input", function(e){
+            const target = e.target; // Pegando o valor do campo de telefone
+            const value = target.value.replace(/\D/g, ''); // Removendo todos os caracteres que não são dígitos
+
+            if(value.length > 0) {
+                if(value.length < 2) {
+                    value = `(${value}`; // Adicionando ( antes do DDD
+                } else if(value.length < 7) {
+                    value = `(${value.substring(0, 2)}) ${value.substring(2)}`; // adicionando ) e espaço após o DDD
+                } else {
+                    value = `(${value.substring(0, 2)}) ${value.substring(2, 7)}-${value.substring(7, 11)}`; // adicionando - após os primeiros 5 dígitos
+                }
+            }
+            target.value = value; // Atualizando o valor do campo de telefone com a formatação aplicada
+        })
+    }
+})
